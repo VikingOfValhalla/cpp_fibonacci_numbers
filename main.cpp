a9b801b17ac8271bd805dbfcbf3646dbd3c4ad19
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2022-04-30 16:28:20 -0400

    Updated initial formula for calulating Fib, need to figure out how to caluclate numbers to push in the vector.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> numbers;
+std::vector<int> numbers = { 0,  };
 
-// to calculate fib 
 // fib(n-1) + fib(n-2);
-int calculate_fibonacci(int n)
+void calculate_fibonacci(int n)
 {
-    int n1=0, n2=1;
-    numbers.push_back(n2);
-    for(int x : numbers)
+    if (n<2)
     {
-        numbers[x] = numbers[x] - numbers[x-1];
-        std::cout << x << std::endl;
-    };
-    return n;
+        std::cout << n << std::endl;
+    }
+
+    int n1 = n - 1;
+    int n2 = n - 2;
+    int fib = numbers[n1] + numbers[n2];   
+    std::cout << fib << std::endl;    
 }
 
+
+// main function
 int main ()
 {
-    std::cout << calculate_fibonacci(10) << std::endl;
+    calculate_fibonacci(3);
+
     return 0;
 }
